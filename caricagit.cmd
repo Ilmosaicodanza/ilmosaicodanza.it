1778d0a02ba9408a7dcdf8bec5331b49fdfdfa48
polkabot <panathos@gmail.com>
2022-05-17 17:44:02 +0200

    Orari corsi di swing sistemati

--- caricagit.cmd
+++ caricagit.cmd
@@ -9,7 +9,7 @@ git add . || GOTO :ERRORE
 SET /P MSG=messaggio del commit?
 git commit -m "%MSG%"
 rem git push -u origin main || GOTO :ERRORE
-git push https://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@github.com/Ilmosaicodanza/ilmosaicodanza.it.git|| GOTO :ERRORE
+git push --force -u origin main || GOTO :ERRORE
 color A0
 popd
 ping -n 2 localhost>NUL
