42a390f9b201330053c5d6e129be3096da6af26c
polkabot <panathos@gmail.com>
2022-06-04 14:04:31 +0200

    tolto jquery dalla home page

---
+++ js/S-06.js
@@ -0,0 +1,70 @@
+// particle originali con spread on click
+
+getScript('/js/tsparticles.min.js', function()
+{
+tsParticles.load("tsparticles", {
+  "autoPlay": true,
+  fpsLimit: 120,
+  "interactivity": {
+    "events": {
+      "onClick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "onHover": {
+        "enable": true,
+        "mode": "connect",
+        "parallax": {
+          "force": 60
+        }
+      },
+    },
+  },
+  particles: {
+    color: { 
+      value: "#EBEF9B"
+      //value: "random"
+    },
+    //"links": {
+    //  "color": {
+    //    "value": "#ffffff"
+    //  },
+    //  "distance": 150,
+    //  "opacity": 0.4
+    //},
+    lineLinked: {
+      color: "#ffff00",
+      distance: 80,
+      enable: false,
+      opacity: 0.4,
+      width: 1.0
+    },
+    move: {
+      bounce: false,
+      direction: "none",
+      enable: true,
+      outMode: "out",
+      random: false,
+      speed: 2,
+      straight: false
+    },
+    number: { 
+      density: { enable: false },
+      value: 32
+    },
+    opacity: {
+      animation: { enable: false },
+      random: false,
+      value: 0.5
+    },
+    shape: {
+      type: "circle"
+    },
+    size: {
+      anim: { enable: true },
+      random: true,
+      value: 5
+    }
+  }
+})
+})//end getscript
