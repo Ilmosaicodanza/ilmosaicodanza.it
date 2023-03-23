fdab5186ff64d68da4c0c857ed3c05600867ef76
polkabot <panathos@gmail.com>
2023-03-23 17:33:00 +0100

    js static

--- js/gtag.js
+++ js/gtag.js
@@ -1,6 +1,6 @@
-//Global site tag (gtag.js) - Google Analytics -->
-getScript("https://www.googletagmanager.com/gtag/js?id=G-EBVZR5F3Y8");
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-EBVZR5F3Y8');
+getScript('https://www.googletagmanager.com/gtag/js?id=G-EBVZR5F3Y8', function() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-EBVZR5F3Y8');
+});
