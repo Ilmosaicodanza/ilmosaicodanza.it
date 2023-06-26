30cbe5074ccbf651d6ee3e532920142b9201618d
polkabot <panathos@gmail.com>
2023-06-26 20:34:10 +0200

    Aggiunti gli orari di flamenco ai corsi estivi

--- js/smooth.js
+++ js/smooth.js
@@ -1,6 +1,4 @@
 const L0 = document.querySelectorAll('a[href^="#"]:not([href^="#fig"]):not([href^="#_"])');
-//const L0 = document.querySelectorAll('a:not(figure) [href^="#"]');
-//const L0 = document.querySelectorAll('a:not(figure) [href^="#"]');
 L0.forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
