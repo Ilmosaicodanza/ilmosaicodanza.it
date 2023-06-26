748f7bb1bb55b9433a957eb1dd606dd557ef6234
polkabot <panathos@gmail.com>
2023-06-26 13:06:29 +0200

    /spettacolo-flamenco-huellas-de-tu-ser-2022/, /spettacolo-flamenco-huellas-de-tu-ser-2022/1-introduzione/, /spettacolo-flamenco-huellas-de-tu-ser-2022/2-chiavi-in-mano/, /spettacolo-flamenco-huellas-de-tu-ser-2022/3-rispetto-del-corpo/, /spettacolo-flamenco-huellas-de-tu-ser-2022/4-spettacolo/, /spettacolo-flamenco-huellas-de-tu-ser-2022/5-cuadro-musical/, Aggiornati orari di Luglio in danza con bollywood, oriental e pizzica Pagine: /spettacolo-flamenco-huellas-de-tu-ser-2022/, /spettacolo-flamenco-huellas-de-tu-ser-2022/1-introduzione/, /spettacolo-flamenco-huellas-de-tu-ser-2022/2-chiavi-in-mano/, /spettacolo-flamenco-huellas-de-tu-ser-2022/3-rispetto-del-corpo/, /spettacolo-flamenco-huellas-de-tu-ser-2022/4-spettacolo/, /spettacolo-flamenco-huellas-de-tu-ser-2022/5-cuadro-musical/,

--- js/smooth.js
+++ js/smooth.js
@@ -1,4 +1,6 @@
-const L0 = document.querySelectorAll('a[href^="#"]');
+const L0 = document.querySelectorAll('a[href^="#"]:not([href^="#fig"]):not([href^="#_"])');
+//const L0 = document.querySelectorAll('a:not(figure) [href^="#"]');
+//const L0 = document.querySelectorAll('a:not(figure) [href^="#"]');
 L0.forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
