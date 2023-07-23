ff9f6f165580d3eb260f2b5587c466e111f76d5b
polkabot <panathos@gmail.com>
2023-07-24 01:32:30 +0200

    ricerca con spinner per pause

--- search/orama.mjs
+++ search/orama.mjs
@@ -4,23 +4,33 @@ import {
   insert,
 } from "https://unpkg.com/@orama/orama@latest/dist/index.js";
 
-//let isInitialSearch = true; // Variabile per tenere traccia se la ricerca iniziale è stata eseguita
+let showSpinner;
+let hideSpinner;
 
 const performSearch = async (searchEngine, searchTerm) => {
   const searchResponse = await search(searchEngine, {
     term: searchTerm,
     properties: "*",
   });
-  //isInitialSearch = false; // Imposta la variabile a true per evitare ulteriori ricerche iniziali
   document.getElementById("search-results").innerHTML = searchResponse.hits
     .map(
       (i) =>
-        `<li><a href="${i.document.uri}" class="list-group-item list-group-item-action">${i.document.title}</a></li>`
+        `<li><a href="${i.document.url}" class="list-group-item list-group-item-action">${i.document.title}</a></li>`
     )
     .join("");
+    hideSpinner();
 };
-
 const init = async () => {
+   showSpinner = () => {
+    const overlay = document.getElementById("loading-overlay");
+    overlay.style.display = "block";
+  };
+   hideSpinner = () => {
+    const overlay = document.getElementById("loading-overlay");
+    overlay.style.display = "none";
+  };
+
+  showSpinner();
   const searchEngine = await create({
     schema: {
       title: "string",
@@ -30,12 +40,13 @@ const init = async () => {
     defaultLanguage: "italian",
   });
 
-  const indexResponse = await fetch("index.json");
+  const indexResponse = await fetch("/index.json");
   const index = await indexResponse.json();
 
   for (const item of index) {
     await insert(searchEngine, item);
   }
+  hideSpinner();
 
   const urlSearchParams = new URLSearchParams(window.location.search);
   const searchTerm = urlSearchParams.get("q");
@@ -47,10 +58,9 @@ const init = async () => {
   });
 
   if (searchTerm) {
-    // Se c'è una query string nell'URL e la ricerca iniziale non è ancora stata eseguita
+    showSpinner();
     searchInput.value = decodeURIComponent(searchTerm); // Imposta il valore del campo search-input con il termine cercato
     await performSearch(searchEngine, searchTerm);
-
   }
 };
 
