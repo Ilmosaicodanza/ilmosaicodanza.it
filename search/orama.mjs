1c4e1d1ce789662be5cb2822d7334b81acf15e66
polkabot <panathos@gmail.com>
2023-07-31 19:56:58 +0200

    /lindy-hop/, /roberto-angelica-insegnante-tango/, pagina insegnante Roberto Angelica, /lindy-hop/, /roberto-angelica-insegnante-tango/, tolto logo mosaico da consent.html (richiesta di Marta su WhatsApp), /lindy-hop/, /roberto-angelica-insegnante-tango/, pagina insegnante Roberto Angelica, /lindy-hop/, /roberto-angelica-insegnante-tango/, pagina Roberto Angelica Pagine: /lindy-hop/, /roberto-angelica-insegnante-tango/, pagina insegnante Roberto Angelica, /lindy-hop/, /roberto-angelica-insegnante-tango/, tolto logo mosaico da consent.html (richiesta di Marta su WhatsApp), /lindy-hop/, /roberto-angelica-insegnante-tango/, pagina insegnante Roberto Angelica, /lindy-hop/, /roberto-angelica-insegnante-tango/,

--- search/orama.mjs
+++ search/orama.mjs
@@ -1,19 +1,8 @@
-import {
-  create,
-  search,
-  insert,
-  insertMultiple,
-//} from "./orama/lib/index.js";
-} from "https://unpkg.com/@orama/orama@latest/dist/index.js";
-
+import {create,search,insertMultiple} from "https://unpkg.com/@orama/orama@latest/dist/index.js";
 let showSpinner;
 let hideSpinner;
-
 const heading = document.getElementsByClassName('lh-copy')[0];
-      if (heading) {
-        heading.scrollIntoView();
-      }
-
+if (heading) {heading.scrollIntoView();}
 const performSearch = async (searchEngine, searchTerm) => {
   const searchResponse = await search(searchEngine, {
     term: searchTerm,
@@ -28,15 +17,14 @@ const performSearch = async (searchEngine, searchTerm) => {
     hideSpinner();
 };
 const init = async () => {
-   showSpinner = () => {
+  showSpinner = () => {
     const overlay = document.getElementById("loading-overlay");
     overlay.style.display = "block";
   };
-   hideSpinner = () => {
+  hideSpinner = () => {
     const overlay = document.getElementById("loading-overlay");
     overlay.style.display = "none";
   };
-
   showSpinner();
   const searchEngine = await create({
     schema: {
@@ -47,31 +35,21 @@ const init = async () => {
     },
     defaultLanguage: "italian",
   });
-
   const indexResponse = await fetch("/index.json");
   const index = await indexResponse.json();
-
   await insertMultiple(searchEngine, index);
-  //for (const item of index) {
-  //  await insert(searchEngine, item);
-  //}
-
   hideSpinner();
-
   const urlSearchParams = new URLSearchParams(window.location.search);
   const searchTerm = urlSearchParams.get("q");
-
   const searchInput = document.getElementById("search-input");
   searchInput.addEventListener("input", async (event) => {
     const searchTerm = event.target.value;
     await performSearch(searchEngine, searchTerm);
   });
-
   if (searchTerm) {
     showSpinner();
     searchInput.value = decodeURIComponent(searchTerm); // Imposta il valore del campo search-input con il termine cercato
     await performSearch(searchEngine, searchTerm);
   }
 };
-
 init();
