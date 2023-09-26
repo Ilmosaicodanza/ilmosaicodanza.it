56613d115e163225f54231a09c990e5916aa8caa
polkabot <panathos@gmail.com>
2023-09-26 14:51:43 +0200

    /heels-dance-reggaeton-corsi-ragazzi/, creato tag Podcast Flamenco chiavi in mano, solo per pagine con video specifici della playlist, /heels-dance-reggaeton-corsi-ragazzi/, /flamenco/, /baile-sabina/, /orari-corsi-flamenco/, /cose-il-flamenco/, /con-mucho-compas/, /news/corso-intensivo-di-teoria-del-flamenco-2023-10-05/, introdotte label per podcast nei listing Pagine: /heels-dance-reggaeton-corsi-ragazzi/, creato tag Podcast Flamenco chiavi in mano, solo per pagine con video specifici della playlist, /heels-dance-reggaeton-corsi-ragazzi/, /flamenco/, /baile-sabina/, /orari-corsi-flamenco/, /cose-il-flamenco/, /con-mucho-compas/, /news/corso-intensivo-di-teoria-del-flamenco-2023-10-05/,

--- js/gtag.js
+++ js/gtag.js
@@ -1,3 +1,4 @@
+// static/js
 getScript('https://www.googletagmanager.com/gtag/js?id=G-EBVZR5F3Y8', function() {
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
