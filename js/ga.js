e7552b64f6fc530373fa51287bd97b3b624bcab1
polkabot <fabriconti68@gmail.com>
2025-05-15 19:21:40 +0200

    /news/laboratorio-teatro-saggio-2025/, Fatta pagina news teatro  Pagine: /news/laboratorio-teatro-saggio-2025/,

--- js/ga.js
+++
@@ -1,8 +0,0 @@
-//Global site tag (gtag.js) - Google Analytics -->
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-EBVZR5F3Y8');
-</script>
-<script async defer src="https://www.googletagmanager.com/gtag/js?id={{ . }}"></script>
